88e4386a49bf5c5757809bf5bb17442a64df01c5
Handry Wahyudi <wahyudihandry@gmail.com>
2017-04-29 23:28:03 +0700

    first commit

---
+++ models.py
@@ -0,0 +1,28 @@
+import sqlite3
+
+def drop_table():
+	with sqlite3.connect('songs.db') as connections:
+		c = connections.cursor()
+		c.execute("""DROP TABLE IF EXISTS songs;""")
+	return True
+
+
+def create_db():
+	with sqlite3.connect('songs.db') as connection:
+		c = connection.cursor()
+		table = """CREATE TABLE songs(
+				id INTEGER PRIMARY KEY AUTOINCREMENT,
+				artist TEXT NOT NULL,
+				title TEXT NOT NULL,
+				rating INTEGER NOT NULL
+		);
+		"""
+		c.execute(table)
+	return True
+
+
+if __name__ == '__main__':
+	drop_table()
+	create_db()
+
+	
